a28b75a821d45015ba84cdb6c6e024d5ba8e8ac2
samarth9702 <s223828778@deakin.edu.au>
2024-04-13 13:22:32 +1000

    add: 2.2c task

--- src/main/java/sit707_week2/SeleniumOperations.java
+++ src/main/java/sit707_week2/SeleniumOperations.java
@@ -4,20 +4,14 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.By;
+import org.openqa.selenium.*;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
 
-/**
- * This class demonstrates Selenium locator APIs to identify HTML elements.
- * 
- * Details in Selenium documentation https://www.selenium.dev/documentation/webdriver/elements/locators/
- * 
- * @author Ahsan Habib
- */
 public class SeleniumOperations {
 
 	public static void sleep(int sec) {
@@ -95,6 +89,7 @@ public class SeleniumOperations {
 		
 		driver.close();	
 	}
+	
 	public static void amazon_registration_page(String url) {
 		System.setProperty("webdriver.chrome.driver", "E://chromedriver-win64/chromedriver.exe");
 		System.out.println("Fire up chrome browser.");
@@ -126,5 +121,72 @@ public class SeleniumOperations {
 		sleep(2);
 		driver.close();
 		
+	}
+	
+	public static void officeworks_registration_page_relativelocator(String url) {
+		// Step 1: Locate chrome driver folder in the local drive.
+		System.setProperty("webdriver.chrome.driver", "E://chromedriver-win64/chromedriver.exe");
+		
+		// Step 2: Use above chrome driver to open up a chromium browser.
+		System.out.println("Fire up chrome browser.");
+		WebDriver driver = new ChromeDriver();
+		
+		System.out.println("Driver info: " + driver);
+		
+		sleep(2);
+	
+		// Load a webpage in chromium browser.
+		driver.get(url);
+		driver.manage().window().maximize();
+		
+		// Find first input field which is firstname
+		By firstName = RelativeLocator.with(By.tagName("input")).above(By.id("lastname"));
+		WebElement fName = driver.findElement(firstName);
+		fName.sendKeys("Samarth");
+
+		By lastName = RelativeLocator.with(By.tagName("input")).below(By.id("firstname"));
+		WebElement lName = driver.findElement(lastName);
+		lName.sendKeys("Shah");
+		
+		By pNumber = RelativeLocator.with(By.tagName("input")).below(By.id("lastname"));
+		WebElement phoneNumber = driver.findElement(pNumber);
+		phoneNumber.sendKeys("0431864282");
+		
+		By mail = RelativeLocator.with(By.tagName("input")).below(By.xpath("//*[@id=\"phoneNumber\"]"));
+		WebElement email = driver.findElement(mail);
+		email.sendKeys("samxyz@gmail.com");
+		
+		By pass = RelativeLocator.with(By.tagName("input")).below(By.id("email"));
+		WebElement password = driver.findElement(pass);
+		password.sendKeys("Samarth@123");
+		
+		By cPass = RelativeLocator.with(By.tagName("input")).below(By.id("password"));
+		WebElement rPass = driver.findElement(cPass);
+		rPass.sendKeys("Samarth@123");
+		
+		/*
+		 * Identify button 'Create account' and click to submit using Selenium API.
+		 */
+		// Write code
+		By eKey = RelativeLocator.with(By.tagName("button")).below(By.xpath("/html/body/div[2]/div/div[1]/div/div/form/div[10]/div/button[1]"));
+		WebElement enterKey = driver.findElement(eKey);
+		enterKey.click();
+		sleep(3);
+		/*
+		 * Take screenshot using selenium API.
+		 */
+		// Write code
+		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(src, new File("C://Users//Samarth//Desktop//Selenium Screenshot/Screen.png"));
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+		
+		// Sleep a while
+		sleep(2);
+		
+		driver.close();	
 	}
 }
